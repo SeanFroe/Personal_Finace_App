7643feeaf10f829aeb6c6f98b09a0ca9ca55eada
SeanPfroelich@gmail.com <SeanPfroelich@gmail.com>
2025-02-27 16:36:17 -0500

    ADD BudgetCard.jsx and utils.jsx w/currencyFormatter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Container, Stack, Button } from "react-bootstrap";
 import BackendDataComponent from "./components/BackendDataComponent";
+import BudgetCard from "./components/BudgetCard";
 
 function App() {
   return (
     <Container className="my-4">
       <Stack direction="horizontal" gap={2} className="mb-4">
         <h1 className="me-auto"> Budgets</h1>
-        <Button varient="primary">Add Budget</Button>
-        <Button varient="outline-primary">Add Expense</Button>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
       </Stack>
       <div
         style={{
@@ -17,7 +18,9 @@ function App() {
           gap: "1rem",
           alignItems: "flex-start",
         }}
-      ></div>
+      >
+        <BudgetCard name="Entertainment" amount={200} max={1000}></BudgetCard>
+      </div>
     </Container>
   );
 }
