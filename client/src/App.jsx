e0428ba19302acc553509cde6ec7be302c4bb4ca
SeanPfroelich@gmail.com <SeanPfroelich@gmail.com>
2025-02-27 15:51:22 -0500

    UPDATE and style Budget Container in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { Container, Stack } from "react-bootstrap";
+import { Container, Stack, Button } from "react-bootstrap";
 import BackendDataComponent from "./components/BackendDataComponent";
 
 function App() {
   return (
-    <Container>
+    <Container className="my-4">
       <Stack direction="horizontal" gap={2} className="mb-4">
         <h1 className="me-auto"> Budgets</h1>
-        <BackendDataComponent />
+        <Button varient="primary">Add Budget</Button>
+        <Button varient="outline-primary">Add Expense</Button>
       </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      ></div>
     </Container>
   );
 }
